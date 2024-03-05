4ed43941181343bd680834d3ee4cfd6dfabadd58
Alejandra <alejandraandrade1224@gmail.com>
2024-03-05 21:32:00 +0100

    Renombrar la clase Circulo por Circunferencia

---
+++ src/main/java/figuras/Circunferencia.java
@@ -0,0 +1,47 @@
+package figuras;
+
+public class Circunferencia {
+
+    private double rad;
+
+    /**
+     * @param rad the rad to set
+     */
+    public void setRad(double rad) {
+        this.rad = rad;
+    }
+
+    public Circunferencia(double radio) {
+        this.rad = radio;
+    }
+
+    public void imprimir() {
+        String color = "rojo";
+        System.out.println("Di�metro: " + 2 * getRad());
+        System.out.println("Color: " + color);
+        double area = 2 * 3.1416 * getRad() * getRad();
+        System.out.println(area);
+    }
+
+    public boolean esIgual(Circunferencia otro, boolean conDecimales) {
+        double radio1 = this.getRad();
+        double radio2 = otro.getRad();
+        if (conDecimales) {
+            if (radio1 == radio2) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (Math.abs(radio1 - radio2) < 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    private double getRad() {
+        return rad;
+    }
+}
